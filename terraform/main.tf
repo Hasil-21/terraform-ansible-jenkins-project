17545063981d8d4d92de1391e7c8f2785b93a401
Hasil-21 <hasiltrivedi21@gmail.com>
2026-07-24 06:53:46 +0000

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,14 @@ terraform{
       version = "~> 2.0"
     }
   }
+  
+  backend "s3"{
+    bucket = "hasil-terraform-state-2026"
+    key = "jenkins-mini-project/terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
+  }
 }
 
 
