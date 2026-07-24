c9c9100f74d7c6dc5a624e81a991f8b33373c452
Hasil-21 <hasiltrivedi21@gmail.com>
2026-07-24 07:23:23 +0000

    changes

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -2,7 +2,7 @@ resource "local_file" "ansible_inventory" {
   filename = "${path.module}/../ansible/inventory_generated.ini"
   content = <<-EOT
     [app]
-    appserver ansible_host=${aws_instance.app.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/DemoKeyPair.pem
+    appserver ansible_host=${aws_instance.app.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/DemoKeyPair.pem
     
     [jenkins]
     jenkinscontroller ansible_host=${aws_instance.jenkins-controller.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/DemoKeyPair.pem
